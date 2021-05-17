c97f0f5762e64324c7e83de69ae6f4097b808c75
BillyGalbreath <blake.galbreath@gmail.com>
2021-05-17 01:43:36 -0500

    progress

---
+++ public/site2/index.php
@@ -0,0 +1,70 @@
+<!--<?php
+$description = "Purpur is a fork of Paper and Tuinity with the goal of providing new and interesting configuration options, which allow for creating a unique gameplay experience not seen anywhere else";
+$ogimg = "https://purpur.pl3x.net/images/og-background_smol.png"
+?>--><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purpur - Your Minecraft, your way</title>
+    <meta name="title" content="Purpur" />
+    <meta name="description" content="<?=$description?>" />
+    <meta property="twitter:title" content="Purpur" />
+    <meta property="twitter:image" content="<?=$ogimg?>" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:description" content="<?=$description?>" />
+    <meta property="og:title" content="Purpur" />
+    <meta property="og:url" content="https://purpur.pl3x.net" />
+    <meta property="og:description" content="<?=$description?>" />
+    <meta property="og:image" content="<?=$ogimg?>" />
+    <link rel="icon" type="image/x-icon" href="assets/purpur.svg" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <h1>Purpur</h1>
+        <h4>Your Minecraft, your way</h4>
+        <img class="logo" src="assets/purpur.svg" alt="Purpur Logo" />
+        <a href="downloads" class="button download-button blue-button">Download</a>
+        <p>Purpur is a drop in replacement for any Spigot/Paper server designed to bring
+         you superior configuration, ultimate gameplay features, and the best blazing fast optimizations.</p>
+      </div>
+    </header>
+    <div class="row-one">
+      <div class="container group">
+        <div class="block">
+          <img class="icon" src="assets/documentation.svg" alt="Fully Documented" />
+          <h3>Fully Documented</h3>
+          <p>Read over our comprehensive feature documentation and make it your own.</p>
+          <a class="button white-button" href="docs">Learn More</a>
+        </div>
+        <div class="block">
+          <img class="icon" src="assets/opensource.svg" alt="Free and Open Source" />
+          <h3>Free and Open Source</h3>
+          <p>Built with love by people of all walks of life all over the globe.</p>
+          <a class="button white-button" href="github" target="_blank">GitHub</a>
+        </div>
+      </div>
+    </div>
+    <div class="row-two">
+      <div class="container group">
+        <div class="block">
+          <img class="icon" src="assets/warning.svg" alt="Found a bug? File it!" />
+          <h3>Found a Bug? File it!</h3>
+          <p>If you run into issues or have any suggestions, let us know.</p>
+          <a class="button red-button" href="issues" target="_blank">Tickets</a>
+        </div>
+        <div class="block">
+          <img class="icon" src="assets/discord.svg" alt="Get Involved" />
+          <h3>Get Involved</h3>
+          <p>Get in touch with the developers, ask questions, and join the fun.</p>
+          <a class="button blue-button" href="discord" target="_blank">Discord</a>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <a href="https://pl3x.net">&copy; 2021 pl3x.net</a>
+    </footer>
+  </body>
+</html>
