1a7cc09e9b1f7e1e058741e18eb6bc8c873674c7
BillyGalbreath <blake.galbreath@gmail.com>
2021-05-18 05:38:37 -0500

    open graph

---
+++ public/opengraph.php
@@ -0,0 +1,6 @@
+<?php
+$title = "Purpur - Your Minecraft, your way";
+$description = "Purpur is a drop in replacement with the goal of providing new and interesting configurations, which allow for creating a unique gameplay experience";
+$ogimg = "https://purpur.pl3x.net/card.png"
+?>
+
