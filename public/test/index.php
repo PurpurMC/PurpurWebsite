56eeb8a11355f9deb886e5f4310a87ee5c833150
BillyGalbreath <blake.galbreath@gmail.com>
2021-05-24 08:12:59 -0500

    basic header

--- public/test/index.php
+++ public/test/index.php
@@ -22,6 +22,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Purpur - Your Minecraft, your way</title>
 
@@ -48,10 +49,37 @@
       window.onload = (event) => {
         const params = new URLSearchParams(window.location.search);
         document.getElementById(params.get("id")).scrollIntoView();
+        fetch("https://api.github.com/repos/pl3xgaming/purpurdocs")
+          .then(async res => {
+            if (res.ok) {
+              const json = await res.json();
+              console.log(json);
+              const span = document.getElementById("gh_stats");
+              const forks = json['forks_count'];
+              const stars = json['stargazers_count'];
+              span.innerHTML = `${stars} Stars · ${forks} Forks`;
+            }
+          });
       };
     </script>
   </head>
   <body>
+    <header>
+      <div class="container">
+        <a class="logo" href="/test"><img src="/images/purpur.svg" alt="Purpur Documentation" /></a>
+        <a class="navbtn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2z"></path></svg></a>
+        <p>Purpur Documentation</p>
+        <a class="purpurdocs" href="">
+          <div class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M439.55 236.05L244 40.45a28.87 28.87 0 00-40.81 0l-40.66 40.63 51.52 51.52c27.06-9.14 52.68 16.77 43.39 43.68l49.66 49.66c34.23-11.8 61.18 31 35.47 56.69-26.49 26.49-70.21-2.87-56-37.34L240.22 199v121.85c25.3 12.54 22.26 41.85 9.08 55a34.34 34.34 0 01-48.55 0c-17.57-17.6-11.07-46.91 11.25-56v-123c-20.8-8.51-24.6-30.74-18.64-45L142.57 101 8.45 235.14a28.86 28.86 0 000 40.81l195.61 195.6a28.86 28.86 0 0040.8 0l194.69-194.69a28.86 28.86 0 000-40.81z"/></svg>
+          </div>
+          <div class="github">
+            PurpurDocs
+            <span id="gh_stats"><?=$gh_stars?> Stars · <?=$gh_forks?> Forks</span>
+          </div>
+        </a>
+      </div>
+    </header>
     <div class="configuration">
 <?php
   buildConfig($json, null);
