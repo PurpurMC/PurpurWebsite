fb9a6f3ade43378db675e2793fe9a2f5eb8d2f6c
Encode42 <me@encode42.dev>
2021-12-30 00:38:45 -0500

    Update downloads.php

--- public/downloads.php
+++ public/downloads.php
@@ -5,7 +5,7 @@
 
     $allKnownVulnerabilities = [
         [
-                "affectedVersions"=>["1.17.1", "1.17", "1.16.5", "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.16", "1.15.2", "1.15.1", "1.15", "1.14.4", "1.14.3", "1.14.2", "1.14.1"],
+                "affectedVersions"=>["1.18", "1.17.1", "1.17", "1.16.5", "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.16", "1.15.2", "1.15.1", "1.15", "1.14.4", "1.14.3", "1.14.2", "1.14.1"],
                 "message"=>"This version is known to contain an RCE vulnerability!<br /><a href='https://purpurmc.org/docs/Log4j/'>Read about the exploit and potential mitigations here</a>"
         ]
     ];
