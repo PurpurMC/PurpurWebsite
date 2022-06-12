e5f4969e28e567c1b361bc7655616d0b1538557f
BillyGalbreath <BillyGalbreath@users.noreply.github.com>
2022-06-12 16:41:14 -0500

    Remove experimental tag

--- public/downloads.php
+++ public/downloads.php
@@ -2,7 +2,7 @@
     require_once("inc/versions.php");
 
     $allHotfixes = [];
-    $betaVersions = ["1.19"];
+    $betaVersions = [];
     $allKnownVulnerabilities = [
         [
                 "affectedVersions"=>["1.18", "1.17.1", "1.17", "1.16.5", "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.16", "1.15.2", "1.15.1", "1.15", "1.14.4", "1.14.3", "1.14.2", "1.14.1"],
