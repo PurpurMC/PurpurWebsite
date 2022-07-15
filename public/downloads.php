7cd68c504c3a0acd21686ae726a29801e8433a8d
Ben Kerllenevich <ben@omega24.dev>
2022-07-14 22:26:18 -0400

    use papyrus v2

--- public/downloads.php
+++ public/downloads.php
@@ -1,119 +1,74 @@
 <?php
     require_once("inc/versions.php");
 
-    $allHotfixes = [];
-    $betaVersions = [];
-    $allKnownVulnerabilities = [
-        [
-                "affectedVersions"=>["1.18", "1.17.1", "1.17", "1.16.5", "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.16", "1.15.2", "1.15.1", "1.15", "1.14.4", "1.14.3", "1.14.2", "1.14.1"],
-                "message"=>"This version is known to contain an RCE vulnerability!<br /><a href='https://purpurmc.org/docs/Log4j/'>Read about the exploit and potential mitigations here</a>"
-        ]
-    ];
+    $url = "https://api.purpurmc.org/v2/purpur";
 
-    $contents = file_get_contents("/srv/papyrus/data.json");
-    $json = json_decode($contents, true);
+    $project = file_get_contents($url);
+    $project = json_decode($project, true);
 
-    $project = null;
-    foreach ($json["projects"] as $possibleProject) {
-        if ($possibleProject["name"] == "purpur") {
-            $project = $possibleProject;
-        }
-    }
-
-    $rootVersionNames = [];
-    $versionNames = [];
+    $versions = [];
+    $rootVersions = [];
     foreach ($project["versions"] as $version) {
-        $versionNames[] = $version["name"];
+        $versions[] = $version;
 
-        preg_match("/\d\.\d*/", $version["name"], $matches);
-        $rootVersionNames[] = $matches[0];
+        preg_match("/\d\.\d*/", $version, $matches);
+        $rootVersions[] = $matches[0];
     }
-    rsort($versionNames);
+    $rootVersions = array_unique($rootVersions);
+    rsort($rootVersions);
 
-    $rootVersionNames = array_unique($rootVersionNames);
-    rsort($rootVersionNames);
-
-    $finalVersionNames = [];
-    $visibleTabs = [];
-    foreach ($rootVersionNames as $rootVersion) {
+    $listVersions = [];
+    foreach ($rootVersions as $rootVersion) {
         $subversions = [];
 
-        foreach($versionNames as $version) {
+        foreach($versions as $version) {
             if (str_contains($version, $rootVersion)) {
                 $subversions[] = $version;
             }
         }
-
         usort($subversions, "version_compare");
 
         $finalVersion = $subversions[count($subversions) - 1];
-        $finalVersionNames[] = $finalVersion;
-
         if (!in_array($finalVersion, $forceInvisible)) {
-            $visibleTabs[] = $finalVersion;
+            $listVersions[] = $finalVersion;
         }
+
         if (in_array($finalVersion, $betaVersions)) {
             $finalVersion = $subversions[count($subversions) - 2];
-            $visibleTabs[] = $subversions[count($subversions) - 2];
+            $listVersions[] = $subversions[count($subversions) - 2];
         }
     }
 
-    $versionName = $currentVersion;
+    $currentVersion = $listVersions[0];
     if (isset($_GET["v"])) {
-        $versionName = filter_var($_GET["v"], FILTER_SANITIZE_STRING);
-    }
-
-    if (!in_array($versionName, $versionNames)) {
-        $versionName = $currentVersion;
-    }
-
-    $version = [];
-    foreach ($project["versions"] as $possibleVersion) {
-        if ($possibleVersion["name"] == $versionName) {
-            $version = $possibleVersion;
-        }
-    }
-
-    $builds = [];
-    foreach ($version["builds"] as $build) {
-        array_push($builds, $build);
-    }
-    rsort($builds);
-
-    if (array_key_exists($versionName, $allHotfixes)) {
-        $hotfixesForVersion = $allHotfixes[$versionName];
-        $hotfixBuilds = $hotfixesForVersion["builds"];
-        foreach ($hotfixBuilds as $build) {
-            $build["isHotfix"] = true;
-            array_unshift($builds, $build);
+        $setVersion = filter_var($_GET["v"], FILTER_SANITIZE_STRING);
+        if (in_array($setVersion, $versions)) {
+            $currentVersion = $setVersion;
         }
     }
 
     $disclaimers = [];
-    $currentVersionIndex = array_search($currentVersion, $versionNames);
-    $selectedVersionIndex = array_search($versionName, $versionNames);
-    $isExperimental = in_array($versionName, $betaVersions);
-    if ($isExperimental) {
+    if (in_array($currentVersion, $betaVersions)) {
         $disclaimers[] = "You are trying to download experimental builds!<br /><u>DO NOT</u> use these builds in production, as there may be many bugs and corruption issues.<br />Please report any and all issues you encounter!";
     }
 
-    if ($versionName != $currentVersion && ($selectedVersionIndex > $currentVersionIndex || !in_array($versionName, $finalVersionNames))) {
+    if ($currentVersion != $listVersions[0] && !in_array($currentVersion, $betaVersions)) {
         $disclaimers[] = "You are trying to download builds for an old version of Minecraft!<br />Keep in mind that if you download these builds, you will not receive support.";
     }
 
-    foreach ($allKnownVulnerabilities as $vulnerability) {
-        if (in_array($versionName, $vulnerability["affectedVersions"])) {
+    foreach ($knownVulnerabilities as $vulnerability) {
+        if (in_array($currentVersion, $vulnerability["affectedVersions"])) {
             $disclaimers[] = $vulnerability["message"];
         }
     }
 
-    function getHotfix($version, $build) {
-        return "<a href='https://api.purpurmc.org/hotfixes/$version/purpur-$version-$build.jar' class='button white-button' download='purpur-$version-$build.jar' title='Download build #$build'><span><i class='fas fa-cloud-download-alt'></i> $build</span></a>";
-    }
+    $version = json_decode(file_get_contents($url . "/" . $currentVersion . "/detailed"), true);
+    $builds = (array) $version["builds"]["all"];
+    rsort($builds);
 
-    function getDownloadButton($version, $build, $result) {
+    function getDownloadButton($version, $build, $result, $url) {
         if ($result == "SUCCESS") {
-            return "<a href='https://api.purpurmc.org/v2/purpur/$version/$build/download' class='button white-button' download='purpur-$version-$build.jar' title='Download build #$build'><span><i class='fas fa-cloud-download-alt'></i> $build</span></a>";
+            return "<a href='$url/$version/$build/download' class='button white-button' download='purpur-$version-$build.jar' title='Download build #$build'><span><i class='fas fa-cloud-download-alt'></i> $build</span></a>";
         } else {
             return "<a href='#' class='button disabled-button' title='Failed build #$build'><span><i class='fas fa-times-circle'></i> $build</span></a>";
         }
@@ -128,7 +83,7 @@
         foreach ($commits as $commit) {
             $committer = "\n\n- " . scrub($commit["author"]) . " <" . str_replace(".", "&period;", str_replace("@", "&commat;", scrub($commit["email"]))) . ">";
             $hash = "<a href='https://github.com/PurpurMC/Purpur/commit/" . $commit["hash"] . "' class='hash' rel='noreferrer' target='_blank'>" . substr($commit["hash"], 0, 7) . "</a>";
-            $result .= "<p title='" . shortenGitHubUrls(scrub($commit["title"])) . $committer . "'><span>[$hash]</span> " . parseIssues(scrub(explode("\n", $commit["title"])[0])) . "</p>\n";
+            $result .= "<p title='" . shortenGitHubUrls(scrub($commit["description"])) . $committer . "'><span>[$hash]</span> " . parseIssues(scrub(explode("\n", $commit["description"])[0])) . "</p>\n";
         }
         return $result;
     }
@@ -183,13 +138,13 @@
     <div class="row-one">
         <div class="container">
             <ul class="tabs">
-                <?php foreach ($visibleTabs as $name): ?>
-                    <li class="<?=$name == $versionName ? "selected" : ""?>"><a class="tabLink"><?=$name?></a></li>
+                <?php foreach ($listVersions as $version): ?>
+                    <li class="<?=$version == $currentVersion ? "selected" : ""?>"><a class="tabLink"><?=$version?></a></li>
                 <?php endforeach; ?>
             </ul>
             <select id="dropdown">
-                <?php foreach ($visibleTabs as $name): ?>
-                    <option value="<?=$name?>" <?=$name == $versionName ? "selected" : ""?>><?=$name?></option>
+                <?php foreach ($listVersions as $version): ?>
+                    <option value="<?=$version?>" <?=$version == $currentVersion ? "selected" : ""?>><?=$version?></option>
                 <?php endforeach; ?>
             </select>
             <div class="versionWarning <?=(!empty($disclaimers) ? "visible" : "")?>">
@@ -198,13 +153,13 @@
             <table class="downloads">
                 <thead><tr><td class="left">Build</td><td class="middle">Changes</td><td class="right">Date <label>(24h <input type="checkbox" id="check-24h">)</label></td></tr></thead>
                 <tbody>
-                    <?php foreach ($builds as $build): ?>
-                        <tr>
-                            <td class="left"><?=array_key_exists("isHotfix", $build) ? getHotfix($versionName, $build["build"]) : getDownloadButton($build["version"], $build["build"], $build["result"])?></td>
-                            <td class="mid"><?=getCommits($build["commits"])?></td>
-                            <td class="right timestamp" data-timestamp="<?=$build["timestamp"]?>"></td>
-                        </tr>
-                    <?php endforeach; ?>
+                <?php foreach ($builds as $build): ?>
+                    <tr>
+                        <td class="left"><?=getDownloadButton($build["version"], $build["build"], $build["result"], $url)?></td>
+                        <td class="mid"><?=getCommits($build["commits"])?></td>
+                        <td class="right timestamp" data-timestamp="<?=$build["timestamp"]?>"></td>
+                    </tr>
+                <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
