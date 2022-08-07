166b70bc49cb235f24d5ec3f3f91bf7c6ebecfa5
granny <granny@pl3x.net>
2022-08-07 16:58:21 -0700

    hacky anchor link workaround

--- public/configredirect.php
+++ public/configredirect.php
@@ -1,4 +1,9 @@
 <?php
-echo file_get_contents("docs/Configuration/index.html");
+$pattern = '/(href=")#(.+"\s+class="md-nav__link">)/';
+$replacement = '$1./$2';
+
+$index = preg_replace($pattern, $replacement, file_get_contents("docs/Configuration/index.html"));
+echo $index;
+
 //header('Location: /docs/configuration#'.$_GET['option']);
 //die();
