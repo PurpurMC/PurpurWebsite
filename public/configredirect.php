0f507fe3de79cf930544d86f4599066d553e1cff
granny <granny@pl3x.net>
2022-08-07 18:06:35 -0700

    add regex match for perma link icon

--- public/configredirect.php
+++ public/configredirect.php
@@ -1,6 +1,6 @@
 <?php
-$pattern = '/(href=")#(.+"\s+class="md-nav__link">)/';
-$replacement = '$1./$2';
+$pattern = ['/(href=")#(.+"\s+class="md-nav__link">)/', '/(class="headerlink"\s+href=")#(.+?" title="Permanent link">)/'];
+$replacement = ['$1./$2', '$1./$2'];
 
 $index = preg_replace($pattern, $replacement, file_get_contents("docs/Configuration/index.html"));
 echo $index;
