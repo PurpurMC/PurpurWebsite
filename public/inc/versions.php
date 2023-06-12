eddde294fc3c6e5853d2fe9c414714233a9ef6ab
Ben Kerllenevich <ben@omega24.dev>
2023-06-11 21:32:13 -0400

    stabalize 1.20

--- public/inc/versions.php
+++ public/inc/versions.php
@@ -1,6 +1,6 @@
 <?php
-$betaVersions = ["1.20"];
-$currentVersion = "1.19.4";
+$betaVersions = [];
+$currentVersion = "1.20";
 $forceInvisible = ["1.15.2", "1.14.4"];
 $knownVulnerabilities = [
     [
