3e6fb639dc89c8d54ed083a443fbfadfcd224cc7
granny <contact@granny.dev>
2023-10-05 00:15:16 -0700

    unmark 1.20.2 as experimental

--- public/inc/versions.php
+++ public/inc/versions.php
@@ -1,6 +1,6 @@
 <?php
-$betaVersions = ["1.20.2"];
-$currentVersion = "1.20.1";
+$betaVersions = [];
+$currentVersion = "1.20.2";
 $forceInvisible = ["1.15.2", "1.14.4"];
 $knownVulnerabilities = [
     [
