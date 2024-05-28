e752dd3dfd3afd1160b7556984da76ec2e633197
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-05-28 20:55:54 +0200

    start over

--- public/configredirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pattern = ['/(href=")#(.+"\s+class="md-nav__link">)/', '/(class="headerlink"\s+href=")#(.+?" title="Permanent link">)/'];
-$replacement = ['$1./$2', '$1./$2'];
-
-$index = preg_replace($pattern, $replacement, file_get_contents("docs/purpur/Configuration/index.html"));
-echo $index;
-
-//header('Location: /docs/configuration#'.$_GET['option']);
-//die();
--- public/inc/opengraph.php
+++
@@ -1,4 +0,0 @@
-<?php
-$title = "Purpur - Your Minecraft, your way";
-$description = "Purpur is a drop-in replacement for Paper servers designed for configurability and new, fun, exciting gameplay features.";
-$ogimg = "https://purpurmc.org/card.png";
