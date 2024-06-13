a79ed1cde8a6f7d4a1a4f9c74a73cc77d29a9ad8
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-06-13 20:00:24 +0200

    server side rendering config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://purpurmc.org'
+  site: 'https://purpurmc.org',
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
